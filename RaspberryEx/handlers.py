46aca32a335bafb5419ca0ce780b125f5a03afc3
FinoChen <961156762@qq.com>
2017-04-21 17:22:52 +0800

    change the TMHandler write

--- RaspberryEx/handlers.py
+++ RaspberryEx/handlers.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import module
+import DataBase.DBhandler as DB
 
 handlers = [
     (r'/test', module.test),
     (r'/', module.IndexHandler),
-    (r'/TM', module.TMHandler)
+    (r'/TM', module.TMHandler),
+    (r'/DB',DB.DBhandler)
 
 ]
--- RaspberryEx/module.py
+++ RaspberryEx/module.py
@@ -17,5 +17,7 @@ class TMHandler(tornado.web.RequestHandler):
     '''temperature and moistrue'''
     def get(self):
         TMvalue = TMSensorData.TMData().TMvalue()
-        self.write("当前温度:" + str(TMvalue[0]) + "\n\r" + "当前湿度:" + str(TMvalue[1]))
+        json_TMvalue = json.dumps(TMvalue)
+        #self.write("当前温度:" + str(TMvalue[0]) + "\n\r" + "当前湿度:" + str(TMvalue[1]))
+        self.write(json_TMvalue)
         
